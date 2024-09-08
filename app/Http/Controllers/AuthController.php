53c2619715898a3704cb6d863fb924a84253f3ae
k.panteleev <k.panteleev@ddirection.kz>
2024-09-08 19:53:40 +0500

    updated swagger

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,19 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Global\Response;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Validator;
 
 class AuthController extends Controller
 {
     public function login(Request $request)
     {
-        // Validate props:
-        $request->validate([
-            'login' => 'required',
-            'password' => 'required',
-        ]);
+        $validate = Validator::make(
+            $request->all(),    
+            [
+                'login' => 'required',
+                'password' => 'required',
+            ]);
+
+        if($validate->fails()) {
+            return Response::badRequest($validate->errors()->toArray());
+        }
 
         // Getting props:
         [$login, $password] = [$request->get('login'), $request->get('password')];
