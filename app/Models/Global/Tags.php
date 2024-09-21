19b5353270baf22566192c966bb041e2d05b6f7e
k.panteleev <k.panteleev@ddirection.kz>
2024-09-21 14:58:26 +0500

    fixes

--- app/Models/Global/Tags.php
+++ app/Models/Global/Tags.php
@@ -7,20 +7,12 @@ class Tags
     public static function toString(array $tags) {
         asort($tags);
         return json_encode(
-            array_map(fn ($item) => "[$item]", $tags), 
+            array_map(fn ($item) => "$item", $tags), 
             JSON_UNESCAPED_UNICODE
         );
     }
 
     public static function fromString(string $json) {
-        return array_map(
-            fn ($item) => substr($item, 1, -1), 
-            json_decode(
-                $json, 
-                true, 
-                32, 
-                JSON_UNESCAPED_UNICODE
-            )
-        );
+        return json_decode($json, true, 32);
     }
 }
