60611ec0a563909b1cb3b0c047708be803a474cd
k.panteleev <k.panteleev@ddirection.kz>
2024-10-23 21:31:06 +0500

    Update language capture

--- app/Models/Global/Language.php
+++ app/Models/Global/Language.php
@@ -6,8 +6,21 @@ use Illuminate\Http\Request;
 
 class Language
 {
+    private static $_capturedLang = null;
+
     public static function capture() {
-        $lang = Request::capture()->header('Accept-Language', 'ru');
-        return $lang;
+        if(is_null(Language::$_capturedLang)) {
+            $lang = Request::capture()->header('Accept-Language', 'ru');
+
+            if(str_contains($lang, '-'))
+                $lang = strtolower(explode('-', $lang)[0]);
+
+            if($lang != 'ru' || $lang != 'en' || $lang != 'kk')
+                $lang = 'ru';
+
+            Language::$_capturedLang = $lang;
+        }
+
+        return Language::$_capturedLang;
     }
 }
